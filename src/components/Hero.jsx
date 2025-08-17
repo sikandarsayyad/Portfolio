5e2ca44baddbc4a1732ed58b381e4b3365b5e491
sikandarsayyad <sayyadsikandar475@gmail.com>
2025-08-17 19:30:54 +0530

    Initial commit of Portfolio project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className="hero_content w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start mt-10">
             <h2 className="pb-2 text-4xl tracking-tighter lg:text-7xl text-blue-200">
-              Sikandar Sammad Sayyad
+              Sikandar Sayyad
             </h2>
             <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
